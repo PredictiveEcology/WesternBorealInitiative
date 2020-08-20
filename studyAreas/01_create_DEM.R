d2169a662a4fc5486d8d818d9aa428e4bf1f9e12
Alex Chubaty <achubaty@for-cast.ca>
2020-08-20 18:19:09 -0400

    need Windows to create climate DEMs

--- studyAreas/01_create_DEM.R
+++ studyAreas/01_create_DEM.R
@@ -1,3 +1,6 @@
+## NOTE: DEM raster fires (*.asc) need to be created on Windows for use with ClimateNA
+stopifnot(Sys.info()["sysname"] == "Windows")
+
 source("studyAreas/00_WB_BCR.R")
 
 install_github("PredictiveEcology/climateData@development")
