5be42ddca9877c32c0ebb52aa2da1a5b7f0d3fcd
Alex Chubaty <achubaty@for-cast.ca>
2020-10-07 14:49:10 -0600

    TPI and SWI scripts

---
+++ topography/TPI.R
@@ -0,0 +1,14 @@
+library(raster)
+library(reproducible)
+
+rasterOptions(default = TRUE)
+options("rasterMaxMemory" = 5e12,
+        "rasterTmpDir" = "/mnt/scratch")
+
+dPath <- checkPath("~/Documents/DEMs", create = TRUE)
+
+dem <- prepInputs(url = "https://drive.google.com/file/d/121x_CfWy2XP_-1av0cYE7sxUfb4pmsup",
+                  destinationPath = dPath,
+                  fun = 'raster::raster')
+
+tpi <- terrain(dem, "TPI", filename = file.path(dPath, "tpi.tif"))
