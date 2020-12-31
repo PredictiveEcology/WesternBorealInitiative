1ec544090a95bdb3e2e0ebd31ecee504afadca54
Alex Chubaty <achubaty@for-cast.ca>
2020-12-31 11:51:41 -0700

    add messages to assist debugging raster IO errors

--- studyAreas/02_create_MDC.R
+++ studyAreas/02_create_MDC.R
@@ -9,17 +9,20 @@ climateDataDir <- normalizePath("~/data/climate/ClimateNA_data")
 # Western Boreal ------------------------------------------------------------------------------
 
 mdc_wb1 <- lapply(c(provs, terrs), function(x) {
+  message("Processing historic MDC rasters for study area: ", x)
   dir1 <- file.path(climateDataDir, "historic_1991-2019", x)
   mdc1 <- makeMDC(inputPath = dir1, years = 1991:2019)
   writeRaster(mdc1, file.path("studyAreas/data", paste0("MDC_historic_1991-2019_", x, ".grd")), overwrite = TRUE)
 
-  dir2 <- file.path(climateDataDir, "future_2011-2100", "CCSM4_RCP45", x)
-  mdc2 <- makeMDC(inputPath = dir2, years = 2011:2100)
-  writeRaster(mdc2, file.path("studyAreas/data", paste0("MDC_CCSM4_RCP45_", x, ".grd")), overwrite = TRUE)
+  # message("Processing projected (RCP 4.5) MDC rasters for study area: ", x)
+  # dir2 <- file.path(climateDataDir, "future_2011-2100", "CCSM4_RCP45", x)
+  # mdc2 <- makeMDC(inputPath = dir2, years = 2011:2100)
+  # writeRaster(mdc2, file.path("studyAreas/data", paste0("MDC_CCSM4_RCP45_", x, ".grd")), overwrite = TRUE)
 
-  dir3 <- file.path(climateDataDir, "future_2011-2100", "CCSM4_RCP85", x)
-  mdc3 <- makeMDC(inputPath = dir3, years = 2011:2100)
-  writeRaster(mdc3, file.path("studyAreas/data", paste0("MDC_CCSM4_RCP85_", x, ".grd")), overwrite = TRUE)
+  # message("Processing projected (RCP 4.5) MDC rasters for study area: ", x)
+  # dir3 <- file.path(climateDataDir, "future_2011-2100", "CCSM4_RCP85", x)
+  # mdc3 <- makeMDC(inputPath = dir3, years = 2011:2100)
+  # writeRaster(mdc3, file.path("studyAreas/data", paste0("MDC_CCSM4_RCP85_", x, ".grd")), overwrite = TRUE)
 })
 
 # Ontario -------------------------------------------------------------------------------------
