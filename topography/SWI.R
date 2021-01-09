a108fbef889539116516ef1ea6a2c3bbaabb3e2c
Alex Chubaty <achubaty@for-cast.ca>
2021-01-08 20:48:18 -0700

    minor

--- topography/SWI.R
+++ topography/SWI.R
@@ -7,7 +7,7 @@ dPath <- tempdir()
 
 dem <- prepInputs(url = "https://drive.google.com/file/d/121x_CfWy2XP_-1av0cYE7sxUfb4pmsup",
                   destinationPath = dPath,
-                  fun = 'raster::raster')
+                  fun = "raster::raster")
 
 dem.grd <- writeRaster(dem, file.path(dPath, "dem.grd"))
 dem.saga <- RSAGA::rsaga.import.gdal(file.path(dPath, "dem.grd"), file.path(dPath, "dem.sgrd"))
--- topography/TPI.R
+++ topography/TPI.R
@@ -9,6 +9,6 @@ dPath <- checkPath("~/Documents/DEMs", create = TRUE)
 
 dem <- prepInputs(url = "https://drive.google.com/file/d/121x_CfWy2XP_-1av0cYE7sxUfb4pmsup",
                   destinationPath = dPath,
-                  fun = 'raster::raster')
+                  fun = "raster::raster")
 
 tpi <- terrain(dem, "TPI", filename = file.path(dPath, "tpi.tif"))
