2a494895d98c68d1d8ed8affed23bd4b3e525c4a
Alex Chubaty <achubaty@for-cast.ca>
2021-04-12 22:16:55 -0600

    more tweaks

--- studyAreas/WBI_all.R
+++ studyAreas/WBI_all.R
@@ -6,8 +6,8 @@
 Require("ggmap")
 Require("ggspatial")
 
-cols.bord <- c("blue", "brown", "goldenrod", "darkgreen", "magenta") ## TODO: tweak colours
-cols.fill <- c("lightblue", "brown", "goldenrod", "darkgreen", "pink") ## TODO: tweak colours
+cols.bord <- c("blue", "brown", "purple", "darkgreen", "magenta") ## TODO: tweak colours
+cols.fill <- c("lightblue", "brown", "purple", "darkgreen", "pink") ## TODO: tweak colours
 
 png("figures/all_studyAreas.png", width = 1200, height = 800)
 plot(studyArea)
@@ -20,14 +20,15 @@ dev.off()
 
 ## ggplot version
 alpha <- 0.5
-gg_all <- ggplot(st_as_sf(studyArea)) +
+gg_all <- ggplot(provsWB[-which(provsWB$NAME_1 == "Nunavut"), ]) +
+  geom_sf(fill = "white", colour = "black", alpha = alpha) +
   theme_bw() +
-  geom_sf(fill = "grey", colour = "black", alpha = alpha) +
   annotation_north_arrow(location = "bl", which_north = "true",
-                         pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+                         pad_x = unit(0.25, "in"), pad_y = unit(0.25, "in"),
                          style = north_arrow_fancy_orienteering) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("Western Boreal Initiative Study Areas") +
+  geom_sf(data = st_as_sf(studyArea), fill = "grey", colour = "black", alpha = alpha) +
   geom_sf(data = bcr6_nt1, fill = cols.fill[1], colour = cols.bord[1], alpha = alpha) +
   geom_sf(data = studyAreaMPB, fill = cols.fill[2], colour = cols.bord[2], alpha = alpha) +
   geom_sf(data = studyAreaSBW, fill = cols.fill[3], colour = cols.bord[3], alpha = alpha) +
