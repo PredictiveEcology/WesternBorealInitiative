af36ad6baa64169f2096a10089095de9b87ecf95
Peter Solymos <psolymos@gmail.com>
2025-02-04 22:35:07 -0700

    Explain deployment and trigger only on changes in ./docs
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ Deployment.R
@@ -0,0 +1,8 @@
+# 1. make changes to the source files in the ./website folder
+# 2. render the R Markdown pages
+od <- setwd("website")
+rmarkdown::render_site()
+setwd(od)
+# 3. check the site (./docs folder) in the browser
+# 4. if all looks good, commit the changes - this will trigger the GitHub action
+#    that deploys the content
